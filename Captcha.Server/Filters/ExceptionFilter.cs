253aad737e78f88d1a2709a6129f6ecac6b2a973
Opomomlslsla <opomomlslsla@gmail.com>
2024-10-03 14:46:19 +0300

    exceptionFilteradd

---
+++ Captcha.Server/Filters/ExceptionFilter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Captcha.Domain.Exceptions;
+
+namespace API.Filters;
+
+public class ApiExceptionFilter : ExceptionFilterAttribute
+{
+    public override void OnException(ExceptionContext context)
+    {
+        if (context.Exception is NullEntityException)
+        {
+            context.Result = new NotFoundObjectResult(context.Exception.Message);
+        }
+        else if (context.Exception is DbUpdateException dbUpdateException)
+        {
+            context.Result = new BadRequestObjectResult(dbUpdateException.InnerException?.Message ?? dbUpdateException.Message);
+        }
+        else
+        {
+            context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+        }
+
+        context.ExceptionHandled = true;
+    }
+}
